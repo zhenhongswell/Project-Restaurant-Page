d304a55db7df26a08d963a769608b44a89ba59b3
zhenhongswell <zhenhongswell@gmail.com>
2024-03-21 14:44:07 +0800

    npm script: "publish": "git subtree push --prefix dist origin gh-pages"
    --prefix <subdirectory>：Specify the path of the subdirectory to push
    <remote>：Name of the remote repository
    <branch>：Name of the target branch
    
    how to use in CLI:
    npm run publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { Template } = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Template } = require("webpack");
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template:'./src/index.html',
-            title: 'Output Management',
-        }),
-    ],
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
 
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Output Management",
+    }),
+  ],
 };
